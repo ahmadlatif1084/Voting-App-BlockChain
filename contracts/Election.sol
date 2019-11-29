7c5c87bd7a4c1c885785bc4eb5e55a9b025d0132
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-11-29 18:55:22 +0500

    working 2

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,17 @@
 pragma solidity ^0.5.0;
 
 contract Election {
+//Model Candidate
     struct Candidate {
     uint id;
     string name;
     uint voteCount;
     }
+    //Store accounts that have voted
+    mapping (address => bool) public voters;
+    //Read write candidates
     mapping(uint => Candidate) public candidates;
+    //store candidates count
     uint public candidatesCount;
     constructor() public{
         addCandidate("Candidate 1");
@@ -15,5 +20,18 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name,0);
+    }
+    function vote(uint _candidateId) public{
+        // require that they haven't voted before
+        require(!voters[msg.sender],'require that they donot voted before');
+             //require a valid candidate
+             require(_candidateId > 0 && _candidateId <= candidatesCount,'candidate valid');
+            //record voter has voted
+            voters[msg.sender] = true;
+            //update candidate vote count
+            candidates[_candidateId].voteCount++;
+
+
+
     }
 }
